7547845a05a5ca2dc338e83b44aa2217bf5e9474
17670639SC <58179647+17670639SC@users.noreply.github.com>
2020-03-02 14:20:24 +0000

    Fix

--- Assets/Scripts/CAttack.cs
+++ Assets/Scripts/CAttack.cs
@@ -14,8 +14,11 @@ public class CAttack : MonoBehaviour
     public float[] damageArray = { 0, 0, 0, 0 };
     public float rangedCooldown;
     public float stale = 0f;
-    public float count = 0f;
 
+    private float lightAttackDamage;
+    private float heavyAttackDamage;
+    private float rangedAttackDamage;
+    private float specialAttackDamage;
     private Animator anim;
     private SpecialAttackControl specialAC;
     private Queue<int> moveQueue;
@@ -28,6 +31,11 @@ public class CAttack : MonoBehaviour
         timer = Instantiate(timer, new Vector2(100, 100), Quaternion.identity).GetComponent<TimeControl>();
         timer.countDown = true;
         moveQueue = new Queue<int>(9);
+        lightAttackDamage = damageArray[0];
+        heavyAttackDamage = damageArray[1];
+        rangedAttackDamage = damageArray[2];
+        specialAttackDamage = damageArray[3];
+
     }
 
     // Update is called once per frame
@@ -41,9 +49,21 @@ public class CAttack : MonoBehaviour
             {
                 moveQueue.Dequeue();
             }
+
             moveQueue.Enqueue(1);
-            stale = (StaleMoves(1) / 5);
-            damageArray[0] = damageArray[0] - stale;
+            stale = StaleMoves(1);
+            if (stale == 0)
+            {
+                damageArray[0] = lightAttackDamage;
+            }
+            else
+            {
+                damageArray[0] -= stale / 5;
+                if (damageArray[0] < 1)
+                {
+                    damageArray[0] = 1;
+                }
+            }
             count += 1;
         }
 
@@ -56,6 +76,25 @@ public class CAttack : MonoBehaviour
         {
             heavyPunchCheck.SetActive(true);
             anim.SetTrigger("Heavy Punch");
+            if (moveQueue.Count == 9)
+            {
+                moveQueue.Dequeue();
+            }
+            moveQueue.Enqueue(2);
+            stale = (StaleMoves(2));
+            if (stale == 0)
+            {
+                damageArray[1] = heavyAttackDamage;
+            }
+            else
+            {
+                damageArray[1] -= stale / 2;
+                if (damageArray[1] < 1)
+                {
+                    damageArray[1] = 1;
+                }
+            }
+            count += 1;
         }
 
         if (Input.GetButtonUp("HeavyAttack") && gameObject.tag == "Player")
@@ -72,6 +111,24 @@ public class CAttack : MonoBehaviour
                 proj.tag = "Player";
                 anim.SetTrigger("rangedAttack");
                 timer.time = rangedCooldown;
+                if (moveQueue.Count == 9)
+                {
+                    moveQueue.Dequeue();
+                }
+                moveQueue.Enqueue(3);
+                stale = StaleMoves(3);
+                if (stale == 0)
+                {
+                    damageArray[2] = rangedAttackDamage;
+                }
+                else
+                {
+                    damageArray[2] -= stale / 2.5f;
+                    if (damageArray[2] < 1)
+                    {
+                        damageArray[2] = 1;
+                    }
+                }
             }
         }
 
