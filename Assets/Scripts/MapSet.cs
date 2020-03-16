46600a3889c78adf5bde7989ed6cf38f9043f660
jpps71 <jordan.hayes13@outlook.com>
2020-03-16 02:24:45 +0000

    Map Select Mark 1

---
+++ Assets/Scripts/MapSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSet : MonoBehaviour
+{
+
+    public GameObject mapObj;
+
+    private Map_Array mapInfoScript;
+    private int mapChoice;
+    private Material currentMapMaterial;
+    private MeshRenderer backgroundRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mapInfoScript = mapObj.GetComponent<Map_Array>();
+        mapChoice = mapInfoScript.getMapPlayerChoice();
+        currentMapMaterial = mapInfoScript.mapTextures[mapChoice];
+        backgroundRenderer = this.GetComponent<MeshRenderer>();
+        backgroundRenderer.material = currentMapMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
