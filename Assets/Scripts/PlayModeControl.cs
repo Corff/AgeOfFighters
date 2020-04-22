4d7e85a21014f35345d97f0ccc58f36b4c4b5b85
jpps71 <16654402@students.lincoln.ac.uk>
2020-04-23 00:45:41 +0100

    Import Of Stuff

---
+++ Assets/Scripts/PlayModeControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayModeControl : MonoBehaviour
+{
+
+    public static bool isMultiplayer = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setPlayTypeMultiplayer(bool inType)
+    {
+        isMultiplayer = inType;
+    }
+}
